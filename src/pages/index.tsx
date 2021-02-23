49d93b5f7fd6cdd952e6abc0dddfc34c9cf83f1f
Camila Matos <camila.mila148@gmail.com>
2021-02-23 15:10:20 -0300

    Criado css de estilo para a pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
+import style from '../styles/pages/Home.module.css'
+
 
 export default function Home() {
   return (
-    <div>
+    <div className={style.container}>
       <ExperienceBar />
 
       <section>
