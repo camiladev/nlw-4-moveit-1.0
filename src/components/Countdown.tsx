8882d3a79c1c14374830dc3f413fe308b011a7d4
Camila Matos <camila.mila148@gmail.com>
2021-02-23 15:55:20 -0300

    Criado botao de inicar ciclo

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,16 +2,23 @@ import style from '../styles/components/Countdown.module.css'
 
 export function Countdown() {
     return(
-        <div className={style.countdownContainer}>
-            <div>
-                <span>2</span>
-                <span>5</span>
+        <div>
+            <div className={style.countdownContainer}>
+                <div>
+                    <span>2</span>
+                    <span>5</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>0</span>
+                    <span>0</span>
+                </div>
+                
             </div>
-            <span>:</span>
-            <div>
-                <span>0</span>
-                <span>0</span>
-            </div>
+
+            <button type="button" className={style.countdownButton}>
+                Iniciar um ciclo
+            </button>
         </div>
     )
 }
