0022d23a140fe686a6bc2151a6a18cd1b90716b5
Camila Matos <camila.mila148@gmail.com>
2021-02-24 15:48:39 -0300

    Criado funcionalidade para liberar novo desafio aleatorio

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,11 +1,18 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
+import challenges from '../../challenges.json';
 
+interface Challenge{
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
 
 interface ChallengesContextData{
     level: number;
     currentExperience: number;
     challengesCompleted: number;
+    activeChallenge: Challenge;
     levelUp: () => void;
     startNewChallenge: () => void;
 }
@@ -21,13 +28,17 @@ export function ChallengesProvider({ children }: ChallengesProviderProps){
     const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
 
+    const [activeChallenge, setActiveChallenge] = useState(null);
 
     function levelUp(){
         setLevel(level + 1);
     }
 
     function startNewChallenge(){
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
+        const challenge = challenges[randomChallengeIndex];
 
+        setActiveChallenge(challenge);
     }
 
     return(
@@ -35,6 +46,7 @@ export function ChallengesProvider({ children }: ChallengesProviderProps){
             level,
             currentExperience,
             challengesCompleted,
+            activeChallenge,
             levelUp,
             startNewChallenge
         }}>
