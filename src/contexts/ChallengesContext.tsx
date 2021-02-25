9094616120813aa5331e78af9aae0661ae91c8ca
Camila Matos <camila.mila148@gmail.com>
2021-02-25 10:36:35 -0300

    Incluido notificaçao de novo desafio com audio

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 
 import challenges from '../../challenges.json';
 
@@ -33,7 +33,11 @@ export function ChallengesProvider({ children }: ChallengesProviderProps){
 
     const [activeChallenge, setActiveChallenge] = useState(null);
 
-    const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
+    useEffect(() => {
+        Notification.requestPermission();
+    }, []);
 
     function levelUp(){
         setLevel(level + 1);
@@ -44,6 +48,14 @@ export function ChallengesProvider({ children }: ChallengesProviderProps){
         const challenge = challenges[randomChallengeIndex];
 
         setActiveChallenge(challenge);
+
+        new Audio('/notification.mp3').play();
+
+        if(Notification.permission === 'granted'){
+            new Notification('Novo desafio 🎉', {
+                body: `Valendo ${challenge.amount} xp!`
+            })
+        }
     }
 
     function resetChallenge(){
